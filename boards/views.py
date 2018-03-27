aceea536c574403a3ca5b297ee93e8c136e4f8cb
jainanmol123 <jainanmol810@gmail.com>
2018-03-27 18:59:39 +0530

    updated on 27mar

--- boards/views.py
+++ boards/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from .models import Board
+from django.contrib.auth.models import User
+from django.shortcuts import render,redirect,get_object_or_404
+from .forms import NewTopicForm
+from .models import Board,Topic,Post
 from django.http import Http404
 
 def home(request):
@@ -13,3 +15,23 @@ def board_topics(request,pk):
     except Board.DoesNotExist:
         raise Http404
     return render(request,'boards/topics.html',{'board':board})
+
+def new_topic(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    user = User.objects.first()  # TODO: get the currently logged in user
+    if request.method == 'POST':
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.starter = user
+            topic.save()
+            post = Post.objects.create(
+                message=form.cleaned_data.get('message'),
+                topic=topic,
+                created_by=user
+            )
+            return redirect('board_topics', pk=board.pk)  # redirect to the created topic page
+    else:
+        form = NewTopicForm()
+    return render(request, 'boards/new_topic.html', {'board': board, 'form': form})
