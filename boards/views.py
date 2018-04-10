e2b3c0a032c1df50041c8c8e4c1803e7acefa49e
jainanmol123 <jainanmol810@gmail.com>
2018-04-10 16:57:56 +0530

    updated on 10apr

--- boards/views.py
+++ boards/views.py
@@ -3,11 +3,16 @@ from django.shortcuts import get_object_or_404, redirect, render
 from django.db.models import Count
 from .forms import NewTopicForm,PostForm
 from .models import Board, Post, Topic
+from django.views.generic import UpdateView
+from django.utils import timezone
+from django.utils.decorators import method_decorator
+from django.views.generic import ListView
 
 
-def home(request):
-    boards = Board.objects.all()
-    return render(request, 'boards/home.html', {'boards': boards})
+class BoardListView(ListView):
+    model = Board
+    context_object_name = 'boards'
+    template_name = 'boards/home.html'
 
 
 def board_topics(request, pk):
@@ -61,3 +66,23 @@ def board_topics(request,pk):
     board=get_object_or_404(Board,pk=pk)
     topics=board.topics.order_by('-last_updated').annotate(replies=Count('posts')-1)
     return render(request,'boards/topics.html',{'board':board,'topics':topics})
+
+@method_decorator(login_required,name='dispatch')
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ('message', )
+    template_name = 'edit_post.html'
+    pk_url_kwarg = 'post_pk'
+    context_object_name = 'post'
+
+    def get_queryset(self):
+        queryset=super().get_queryset()
+        return queryset.filter(created_by=self.request.user)
+
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.updated_by = self.request.user
+        post.updated_at = timezone.now()
+        post.save()
+        return redirect('topic_posts', pk=post.topic.board.pk, topic_pk=post.topic.pk)
